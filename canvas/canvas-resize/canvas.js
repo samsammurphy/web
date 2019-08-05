1df20c5c56bba6528020925089c245d8fbae5b84
Sam Murphy <samsammurphy@gmail.com>
2019-08-05 16:27:47 +0100

    canvas

---
+++ canvas/canvas-resize/canvas.js
@@ -0,0 +1,7 @@
+
+// resize canvas to fit the screen
+const canvas = document.querySelector('canvas')
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+console.log(window.innerWidth, window.innerHeight)
